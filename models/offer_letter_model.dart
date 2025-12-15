63aa4b67b771b00a13710f0064fcfd2c5fdceafb
keerthigamathiyazhagan1722-creator <keerthigamathiyazhagan1722@gmail.com>
2025-12-16 00:11:59 +0530

    Add files via upload

---
+++ models/offer_letter_model.dart
@@ -0,0 +1,16 @@
+class OfferLetter {
+final String company;
+final String candidate;
+final String role;
+final String salary;
+final String joiningDate;
+
+
+OfferLetter({
+required this.company,
+required this.candidate,
+required this.role,
+required this.salary,
+required this.joiningDate,
+});
+}
